a19b005868f68747abca559d5a553c927bf3d0f1
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-12-02 13:41:00 -0500

    [FIX]-- Routes established.

--- client/App.jsx
+++ client/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
-import Login from './pages/LogIn.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+// import Arena from './pages/Arena'
+import Login from './pages/Login'
+// import Lesson from './pages/Lesson'
+// import Arena from './pages/Arena'
+// import NotFound from './pages/NotFound'
+import Leaderboard from './pages/Leaderboard'
+
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+    <Routes>
+      {/* <Route path="/battle-arena" element= {<Arena/>}/> */}
+      <Route path="/login" element= {<Login/>}/>
+      {/* <Route path="/login" element= {<Lesson/>}/> */}
+      {/* <Route path="/Arena" element= {<Arena/>}/> */}
+      {/* <Route path="/NotFound" element= {<NotFound/>}/> */}
+      <Route path="/Leaderboard" element= {<Leaderboard/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
-
 export default App
