3dfabf7fe30c82bc158d42583eb310e7df9a15bc
Alexander Irizarry <141542160+supernova147@users.noreply.github.com>
2025-12-04 12:48:58 -0500

    [FIX]-- adjusted routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,24 +1,26 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-// import Arena from './pages/Arena'
-import Login from './pages/LogIn'
-// import Lesson from './pages/Lesson'
 import Arena from './pages/Arena'
-// import NotFound from './pages/NotFound'
+import Login from './pages/LogIn'
+import FAQ from './pages/FAQ'
+// import NotFound from './pages/NotFound' 
 import Leaderboard from './pages/Leaderboard'
-
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
-      {/* <Route path="/battle-arena" element= {<Arena/>}/> */}
-      <Route path="/login" element= {<Login/>}/>
-      {/* <Route path="/login" element= {<Lesson/>}/> */}
-      <Route path="/Arena" element= {<Arena/>}/>
-      {/* <Route path="/NotFound" element= {<NotFound/>}/> */}
+      <Route path="/Login" element= {<Login/>}/>
+      <Route path="/FAQ" element= {<FAQ/>}/>
+      <Route path="/FAQ/:lessonId" element={<FAQ />} />
+      <Route path="/" element= {<Arena/>}/>
+      {/* <Route path="/404" element= {<NotFound/>}/> */}
       <Route path="/Leaderboard" element= {<Leaderboard/>}/>
     </Routes>
+    <Footer/>
     </BrowserRouter>
   )
 }
